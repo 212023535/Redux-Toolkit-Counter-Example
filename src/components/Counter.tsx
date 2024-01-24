75a13f82ab7d4e35bc7c015660e6c215fb8192d7
Rudi Zeeman <rudi.zeeman@swarmdigital.co.za>
2024-01-24 16:36:13 +0200

    [Update] - add new action to increment by amount, add btn to component to dispatch action to increment by 10

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../state/store";
-import { decrement, increment } from "../state/counter/counterSlice";
+import { decrement, increment, incrementByAmount } from "../state/counter/counterSlice";
 
 const Counter = () => {
     const count = useSelector((state: RootState) => state.counter.value);
@@ -10,6 +10,7 @@ const Counter = () => {
         <h2>{count}</h2>
         <div>
             <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(incrementByAmount(10))}>Increment By 10</button>
             <button onClick={() => dispatch(decrement())}>Decrement</button>
         </div>
     </div>;
