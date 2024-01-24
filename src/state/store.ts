29d60c9d7e26afe0ef0220947cf7833ca0a1bc94
Rudi Zeeman <rudi.zeeman@swarmdigital.co.za>
2024-01-24 16:23:00 +0200

    [Update] - add counterSlice to store & add reducers to counterSlice

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counter/counterSlice';
 
 export const store = configureStore({ 
-    reducer: {} 
+    reducer: {
+        // connect the slice to the store so that it can be accessed from anywhere within the app through the Provider
+        counter: counterReducer
+    } 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
